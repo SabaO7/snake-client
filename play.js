4643042c37917a3888362e503a7b507a74b46894
Saba Oji <sabaoji@gmail.com>
2023-05-21 21:54:46 -0400

    refactored the code

--- play.js
+++ play.js
@@ -1,7 +1,11 @@
 const connect = require("./client");
-
-
+const { setupInput, handleUserInput } = require("./input");
 const net = require("net");
+
 
 console.log("Connecting ...");
 connect();
+
+
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
